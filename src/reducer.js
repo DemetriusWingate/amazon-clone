08367bbe912736d5053b89aab3c05681fb6a74d0
Demetrius Wingate <dwingate1991@gmail.com>
2022-12-03 18:08:18 -0500

    added remove from basket function

--- src/reducer.js
+++ src/reducer.js
@@ -14,6 +14,28 @@ const reducer = (state, action) => {
         basket: [...state.basket, action.item],
       };
 
+    case "REMOVE_FROM_BASKET":
+      // Logic to remove items from basket
+
+      // we copy the basket here
+      let newBasket = [...state.basket];
+
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+      if (index >= 0) {
+        // item exists in basket, remove it
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Cant remove product (id: ${action.id} as its not in basket)`
+        );
+      }
+      return {
+        ...state,
+        basket: newBasket,
+      };
+
     default:
       return state;
   }
