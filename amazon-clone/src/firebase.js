7810036dbea7bd67f0f7d475ac66de0494109412
Demetrius Wingate <dwingate1991@gmail.com>
2022-12-18 12:12:30 -0500

    deploying web app

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCA-bnFgao062n8MUhvZi96eZCj7oXr6Xw",
+  authDomain: "clone-caba2.firebaseapp.com",
+  databaseURL: "https://console.firebase.google.com/project/clone-caba2"
+  projectId: "clone-caba2",
+  storageBucket: "clone-caba2.appspot.com",
+  messagingSenderId: "546340645093",
+  appId: "1:546340645093:web:927b28b37da48813fe8c0c",
+  measurementId: "G-HV6LB8DHB7",
+};
